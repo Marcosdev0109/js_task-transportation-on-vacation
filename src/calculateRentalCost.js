6676e7aeb960ac91c7062d890cfaa1ff9addf5bc
Joao Marcos Rodrigues Barbosa <jmarcos010981@gmail.com>
2025-08-25 11:43:12 -0300

    update

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,14 +3,20 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  const dailyRate = 40;
-  let total = days * dailyRate;
 
-  if (days >= 7) {
-    total -= 50;
-  } else if (days >= 3) {
-    total -= 20;
+const DAILY_RATE = 40;
+const MID_TERM_THRESHOLD = 3;
+const LONG_TERM_THRESHOLD = 7;
+const MID_TERM_DISCOUNT = 20;
+const LONG_TERM_DISCOUNT = 50;
+
+function calculateRentalCost(days) {
+  let total = days * DAILY_RATE;
+
+  if (days >= LONG_TERM_THRESHOLD) {
+    total -= LONG_TERM_DISCOUNT;
+  } else if (days >= MID_TERM_THRESHOLD) {
+    total -= MID_TERM_DISCOUNT;
   }
 
   return total;
